44ae11c9ae0cc3b80ff770c78e52512d241bb852
IrynaKr1 <krutsiryna@gmail.com>
2025-07-26 18:21:56 +0200

    Add passengerTransport class

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,13 @@
+/**
+ * Abstract class for all transport
+ * @class Vehicle
+ * 
+ * @param {Object} dimensions - Vehicle dimensions: length, width, height in meters
+ * @param {string} brand - Vehicle brand
+ * @param {string} model - Vehicle model
+ * @param {string} manufactoreDate - Date of manufacture in format DD.MM.YYYY
+ */
+
 class Vehicle {
   constructor(dimensions, brand, model, manufactoreDate) {
     this._dimensions = dimensions;
@@ -15,10 +25,62 @@ class Vehicle {
     return age;
   }
 
+/**
+ * 
+ * @returns {string} Row with brand, model and vehicle age
+ */
+
   getFullInfo() {
     return `Vehicle brand is ${this._brand}, model: ${this._model}, age: ${this.age}`;
   }
 }
 
-const auto = new Vehicle(1, '2', '3', '3.07.2000');
+const auto = new Vehicle(
+  { length: 4.5, width: 1.8, height: 1.6 },
+  'Toyota',
+  'Camry',
+  '12.22.2000'
+);
 console.log(auto.getFullInfo());
+
+class PassengerTransport extends Vehicle {
+  constructor(
+    dimensions,
+    brand,
+    model,
+    manufactoreDate,
+    passengerLimit,
+    passengerCount
+  ) {
+    super(dimensions, brand, model, manufactoreDate);
+    this._passengerLimit = passengerLimit;
+    this._passengerCount = passengerCount;
+  }
+
+  addPassenger() {
+    if (this._passengerCount < this._passengerLimit) {
+      this._passengerCount++;
+      return true;
+    } else {
+      throw new RangeError(
+        `PassengerLimit ${this._passengerLimit}, no free seats`
+      );
+    }
+  }
+
+  getFullInfo() {
+    return `Vehicle brand is ${this._brand}, model: ${this._model}, age: ${this.age}, max seats: ${this._passengerLimit}`;
+  }
+}
+
+const auto1 = new PassengerTransport(
+  { length: 4.5, width: 1.8, height: 1.6 },
+  'Seat',
+  'Altea',
+  '12.22.2009',
+  6,
+  5
+);
+console.log(auto1);
+auto1.addPassenger();
+console.log(auto1.getFullInfo());
